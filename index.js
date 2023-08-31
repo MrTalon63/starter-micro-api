4c27135b4873d41d1a85f8b635cecd7b5b9cbd95
Marcin Czop <mzczop@gmail.com>
2023-08-31 08:56:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    if (req.url === "/favicon.ico") {
+    res.write("Not found");
     res.end();
+    return;
+  }
+  const data = await fetch(`https://cdn.discordapp.com/attachments/1129008542398038086${req.url}`);
+  if (data.status.toString().startsWith("2")) {
+    const buf = await data.arrayBuffer();
+    res.writeHead(data.status, { "Content-Type": data.headers.get("Content-Type") });
+    res.write(Buffer.from(buf));
+    res.end();
+    return;
+  } else {
+    const text = await data.text();
+    res.writeHead(data.status, { "Content-Type": data.headers.get("Content-Type") });
+    res.write(text);
+    res.end();
+    return;
+  }
 }).listen(process.env.PORT || 3000);
